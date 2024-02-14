dccbd43d8b100672f7b68c1ea56786a4df930e42
yellowcatdotcom <valentinagoncarenko975@gmail.com>
2024-02-14 16:03:51 +0300

    Finish commit

--- cgi-bin/create_kk.py
+++ cgi-bin/create_kk.py
@@ -38,7 +38,7 @@ template = '''
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
         </head>
         <body>
-            <h1>Создание артиста</h1>
+            <h1>Создание скидки</h1>
             <a class="h3 link-primary" href="/">На главную</a>
             <div class="ps-4 pe-4">
                 <form method="GET">
